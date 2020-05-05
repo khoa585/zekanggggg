5a63943e513a34254b41f873a9c3709161742664
phong  nguyen <phongit1995@gmail.com>
2020-05-05 16:33:12 +0700

    add redux saga

---
+++ src/reducers/news.js
@@ -0,0 +1,9 @@
+let initalNews = {};
+const newsReducers = (state=initalNews,action)=>{
+    switch (action.type) {
+        default:
+            return state
+            break;
+    }
+}
+export default newsReducers ;
