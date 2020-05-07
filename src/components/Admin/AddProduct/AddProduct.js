81987009cdf97d89e472d6e899b28f553c94eac0
Khoa Hồ Duy <hoduykhoa2k@gmail.com>
2020-05-07 21:07:43 +0700

    'first'

--- src/components/Admin/AddProduct/AddProduct.js
+++ src/components/Admin/AddProduct/AddProduct.js
@@ -1,66 +1,68 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Input, Button, Upload } from 'antd';
-import { UploadOutlined  } from '@ant-design/icons';
+import { UploadOutlined } from '@ant-design/icons';
 import './style.scss';
 function AddProduct(props) {
 
     const fileList = [
-     
-      ];
-      
-      const props2 = {
+
+    ];
+
+    const props2 = {
         listType: 'picture',
         defaultFileList: [...fileList],
         className: 'upload-list-inline',
-      };
-      
+    };
+
     return (
         <Container className="contai-add">
             <div>
                 <span>Tên sản phẩm</span><br />
-                <Input placeholder="Basic usage" />
+                <Input placeholder="Tên sản phẩm" />
             </div>
             <div>
-                <span>Tên sản phẩm</span><br />
-                <Input placeholder="Basic usage" />
+                <span>Giá sản phẩm</span><br />
+                <Input placeholder="Giá sản phẩm" />
             </div>
             <div>
-                <span>Tên sản phẩm</span><br />
-                <Input placeholder="Basic usage" />
+                <span>Sao</span><br />
+                <Input placeholder="Sao" />
             </div>
             <div>
-                <span>Tên sản phẩm</span><br />
-                <Input placeholder="Basic usage" />
+                <span>Chi tiết sản phẩm</span><br />
+                <Input placeholder="Chi tiết sản phẩm" />
             </div>
             <div>
-                <span>Tên sản phẩm</span><br />
-                <Input placeholder="Basic usage" />
+                <span>Ảnh</span><br />
+                <Upload {...props2}>
+                    <Button>
+                        <UploadOutlined /> Upload
+                    </Button>
+                </Upload>
             </div>
             <div>
-                <span>Tên sản phẩm</span><br />
-                <Input placeholder="Basic usage" />
+                <span>Thành phần</span><br />
+                <Input placeholder="Thành phần" />
             </div>
             <div>
-                <span>Tên sản phẩm</span><br />
-                <Input placeholder="Basic usage" />
+                <span>Cách sử dụng</span><br />
+                <Input placeholder="Cách sử dụng" />
             </div>
             <div>
-                <span>Tên sản phẩm</span><br />
-                <Input placeholder="Basic usage" />
+                <span>Chú ý</span><br />
+                <Input placeholder="Chú ý" />
             </div>
-            <div className="clearfix">
-                <div>
-                    <Upload {...props2}>
-                        <Button>
-                            <UploadOutlined /> Upload
-                        </Button>
-                    </Upload>
-                </div>,
-
+            <div>
+                <span>Hạn sử dụng</span><br />
+                <Input placeholder="Hạn sử dụng" />
+            </div>
+            <div>
+                <span>Đánh giá</span><br />
+                <Input placeholder="Đánh giá" />
             </div>
             <div className="submit">
-                <Button type="primary">Primary</Button>
+                <Button type="primary">Submit</Button>
             </div>
         </Container>
     );
