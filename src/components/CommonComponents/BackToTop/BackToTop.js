ad96648ec9177e7b1e7c20df78b0d4f4d6e74359
unknown <hoduykhoa2k@gmail.com>
2020-05-13 20:19:36 +0700

    'fix'

--- src/components/CommonComponents/BackToTop/BackToTop.js
+++ src/components/CommonComponents/BackToTop/BackToTop.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from "react";
 import './style.scss';
-import { UpCircleOutlined } from '@ant-design/icons';
+import { UpOutlined } from '@ant-design/icons';
 const BackToTop = () => {
     const [state, setState] = useState(false)
+    const handleScroll = (e) => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            setState(true)
+        } else {
+            setState(false)
+        }
+    }
     useEffect(() => {
-        document.addEventListener("scroll", function (e) {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                setState(true)
-            } else {
-                setState(false)
-            }
-        });
-    }, [])
+        document.addEventListener("scroll", handleScroll);
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        }
+    }, [state])
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
@@ -23,7 +27,7 @@ const BackToTop = () => {
             {state && (
                 <div className="scroll-to-top">
                     <div onClick={() => scrollToTop()}>
-                        <UpCircleOutlined />
+                        <UpOutlined />
                     </div>
                 </div>
             )}
