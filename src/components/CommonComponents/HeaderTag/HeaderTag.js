97fae2e6af18106ad2725fb88deba7e6a2a9dabd
phong  nguyen <phongit1995@gmail.com>
2020-05-18 15:21:52 +0700

    fix ssl

--- src/components/CommonComponents/HeaderTag/HeaderTag.js
+++ src/components/CommonComponents/HeaderTag/HeaderTag.js
@@ -5,15 +5,15 @@ const HeaderTag = props =>(
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#59B858" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta property="og:title" content={props.title || "HOUSELINK"} />
-        <meta property="title" content={props.title || "HOUSELINK"} />
-        <meta property="og:site_name" content="Houselink" />
-        <meta property="site_name" content="Houselink" />
+        <meta property="og:title" content={props.title || "Zekang"} />
+        <meta property="title" content={props.title || "Zekang"} />
+        <meta property="og:site_name" content="Zekang" />
+        <meta property="site_name" content="Zekang" />
         <meta property="og:type" content="website" />
-        <meta property="image" content={props.image || "https://houselink.com.vn/static/cms/logo.png"} />
-        <meta property="og:image" content={props.image || "https://houselink.com.vn/static/cms/logo.png"} />
-        <meta property="og:description" content={props.description || "The convenient & affordable way to find a construction project"} />
-        <meta property="description" content={props.description || "The convenient & affordable way to find a construction project"} />
+        <meta property="image" content={props.image || "https://zekangg.tk/img/logo.png"} />
+        <meta property="og:image" content={props.image || "https://zekangg.tk/img/logo.png"} />
+        <meta property="og:description" content={props.description || "Sản Phẩm Vẩy nến"} />
+        <meta property="description" content={props.description || "Sản Phẩm Vẩy nến"} />
         {!!props.video && <meta property='og:video' content={props.video} />}
         {!!props.video && <meta property='video' content={props.video} />}
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,vietnamese" rel="stylesheet" />
